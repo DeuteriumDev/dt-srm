3738a6d6737faea091de7227a198d505e8f6bf77
Ben Vincent <benjc.vincent@gmail.com>
2025-01-25 12:14:11 -0600

    Update backend

--- django_api/accounts/views.py
+++ django_api/accounts/views.py
@@ -3,6 +3,8 @@ from accounts.serializers import OrganizationSerializer, CustomUserSerializer
 from rest_access_policy import AccessPolicy
 from rest_access_policy import AccessViewSetMixin
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class GenericAccessPolicy(AccessPolicy):
@@ -25,3 +27,10 @@ class CustomUserViewSet(AccessViewSetMixin, viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     access_policy = GenericAccessPolicy
+
+    @action(detail=False)
+    def me(self, request):
+        return Response(self.get_serializer(request.user, many=False).data)
+
+
+# class MeView(viewsets.ViewSet):
