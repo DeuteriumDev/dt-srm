617c5eb004523f1f7e7b8ca71aae40658b54ba4f
Ben Vincent <benjc.vincent@gmail.com>
2025-03-29 15:05:12 -0600

    Add query param namespacing

--- remix_frontend/app/libs/request.ts
+++ remix_frontend/app/libs/request.ts
@@ -2,7 +2,13 @@ import _ from 'lodash';
 
 type BaseSearchParams = Record<
   string,
-  string | undefined | string[] | number | null | boolean
+  | string
+  | undefined
+  | string[]
+  | number
+  | null
+  | boolean
+  | Record<string, string | undefined | string[] | number | null | boolean>
 >;
 export class RequestHelper {
   private url: URL;
@@ -11,9 +17,17 @@ export class RequestHelper {
   }
 
   public getSearchParams<Params extends BaseSearchParams>(): Params {
-    const params: Record<string, string> = {};
+    const params: BaseSearchParams = {};
     this.url.searchParams.forEach((value, key) => {
-      params[key] = value;
+      if (key.indexOf('-') > -1) {
+        const [ns, nestedKey] = key.split('-');
+        if (_.isEmpty(params[ns])) {
+          params[ns] = {};
+        }
+        (params[ns] as Record<string, string>)[nestedKey] = value;
+      } else {
+        params[key] = value;
+      }
     });
     return params as Params;
   }
@@ -30,6 +44,16 @@ export class RequestHelper {
       }
       if (_.isBoolean(value) || value) {
         return `${prev}&${_.escape(key)}=${_.escape(String(value))}`;
+      }
+      if (_.isObject(value)) {
+        const nameSpacedObject = Object.keys(value).reduce(
+          (obj, k) => {
+            obj[`${key}-${k}`] = value[k];
+            return obj;
+          },
+          {} as Record<string, string>,
+        );
+        return `${prev}&${RequestHelper.parseSearchParams(nameSpacedObject)}`;
       }
       return prev;
     }, '');
