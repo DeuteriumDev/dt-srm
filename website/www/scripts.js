8f73a3f07c8a87549bcf6b36c10dfc3ee6aefa43
Ben Vincent <benjc.vincent@gmail.com>
2025-04-17 10:37:17 -0600

    Add initial landing page w/ hero section

---
+++ website/www/scripts.js
@@ -0,0 +1,21 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const prevButton = document.querySelector('.carousel-btn.prev');
+const nextButton = document.querySelector('.carousel-btn.next');
+
+let currentSlideIndex = 0;
+
+function updateCarousel() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    track.style.transform = `translateX(-${currentSlideIndex * slideWidth}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
