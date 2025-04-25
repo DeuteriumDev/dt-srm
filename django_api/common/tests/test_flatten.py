0edef0521e8940adfc557dc809595e5316ea94ae
Ben Vincent <benjc.vincent@gmail.com>
2025-04-25 10:08:46 -0600

    Add tests for `common`

---
+++ django_api/common/tests/test_flatten.py
@@ -0,0 +1,16 @@
+from common.flatten import flatten
+from django.test import TestCase
+
+
+class FlattenTests(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_flatten(self):
+        self.assertEqual(
+            flatten([[1, 2], [3, 4]]),
+            [1, 2, 3, 4],
+        )
